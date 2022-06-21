253d66e7a30c0c8afa64eba2ab16e8128a0af451
Leonardo C Araújo <ogbeni.leonardo@outlook.com>
2022-06-21 15:18:15 -0300

    Resolução do requisito 08

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Title from './Title';
 
 export default class Missions extends Component {
   render() {
     return (
       <div data-testid="missions">
-        Ainda não há nada
+        <Title headline="Missões" />
       </div>
     );
   }
