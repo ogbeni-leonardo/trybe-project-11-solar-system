55b7e93e893a28a95cbaec58a68936432d301bf9
Leonardo C Araújo <ogbeni.leonardo@outlook.com>
2022-06-21 15:25:45 -0300

    Resolução do requisito 09

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+
 import Title from './Title';
+import MissionsCard from './MissionCard';
 
 export default class Missions extends Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        <MissionsCard
+          name="Nome"
+          year="Year"
+          country="Country"
+          destination="Destination"
+        />
       </div>
     );
   }
