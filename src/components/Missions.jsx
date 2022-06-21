c051519b4fea76d1b10c87de90ebde7d0eed6483
Leonardo C Araújo <ogbeni.leonardo@outlook.com>
2022-06-21 15:16:47 -0300

    Resolução do requisito 07

---
+++ src/components/Missions.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Missions extends Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        Ainda não há nada
+      </div>
+    );
+  }
+}
