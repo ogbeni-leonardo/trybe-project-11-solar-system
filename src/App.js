84208ae4f903f0fffa404cdc8fa2f9428cff4622
Leonardo C Araújo <ogbeni.leonardo@outlook.com>
2022-06-21 14:57:08 -0300

    Resolução do requisito 04

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import React from 'react';
 
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
-import Title from './components/Title';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Header />
-        <SolarSystem>
-          <Title headline="Hello World!" />
-        </SolarSystem>
+        <SolarSystem />
       </div>
     );
   }
--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+
+import Title from './Title';
 
 export default class SolarSystem extends Component {
   render() {
-    const { children } = this.props;
     return (
       <div data-testid="solar-system">
-        {children}
+        <Title headline="Planetas" />
       </div>
     );
   }
 }
-
-SolarSystem.propTypes = {
-  children: PropTypes.node.isRequired,
-};
